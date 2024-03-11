9b8b1da531ea9f3d2da01de8516219365c82e3e0
kainfuon <phuongtkp7@gmail.com>
2024-03-11 16:49:30 +0700

    done w3

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static String check(int x) {
+        if ((x <= 0) || (x > 40)) return "Khong hop le";
+        else if (x < 15) return "7000 VND";
+        else if ((x >= 15) && (x < 25)) {
+            return "8000 VND";
+        } else if ((x >= 25) && (x < 30)) {
+            return "9000 VND";
+        }
+        else return "12000 VND";
+    }
+
+
 }
--- src/TestMain.java
+++ src/TestMain.java
@@ -1,11 +1,44 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
-public class Test {
+
+public class TestMain {
 
     @Test
-    void test1(){
-        String result = Main.check(25);
-        assertEquals("Allowed", result);
+    public void test1(){
+        String result = Main.check(0);
+        assertEquals("Khong hop le", result);
+    }
+
+    @Test
+    public void test2(){
+        String result = Main.check(10);
+        assertEquals("7000 VND", result);
+    }
+
+    @Test
+    public void test3(){
+        String result = Main.check(20);
+        assertEquals("8000 VND", result);
+    }
+
+    @Test
+    public void test4(){
+        String result = Main.check(27);
+        assertEquals("9000 VND", result);
+    }
+
+    @Test
+    public void test5() {
+        String result = Main.check(35);
+        assertEquals("12000 VND", result);
+
+    }
+
+    @Test
+    public void test6() {
+        String result = Main.check(45);
+        assertEquals("Khong hop le", result);
+
     }
 
 }
