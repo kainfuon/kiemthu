c9ae5f9d6252c8174c68a367f6368bd81222c522
kainfuon <phuongtkp7@gmail.com>
2024-03-20 20:16:22 +0700

    done w4

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static String check(int x) {
         if ((x <= 0) || (x > 40)) return "Khong hop le";
         else if (x < 15) return "7000 VND";
-        else if ((x >= 15) && (x < 25)) {
+        else if  (x < 25) {
             return "8000 VND";
-        } else if ((x >= 25) && (x < 30)) {
+        } else if (x < 30) {
             return "9000 VND";
         }
         else return "12000 VND";
---
+++ src/TestMain3.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class TestMain3 {
+
+    @Test
+    public void test1(){
+        String result = Main.check(0);
+        assertEquals("Khong hop le", result);
+    }
+
+    @Test
+    public void test2(){
+        String result = Main.check(10);
+        assertEquals("7000 VND", result);
+    }
+
+    @Test
+    public void test3(){
+        String result = Main.check(20);
+        assertEquals("8000 VND", result);
+    }
+
+    @Test
+    public void test4(){
+        String result = Main.check(27);
+        assertEquals("9000 VND", result);
+    }
+
+    @Test
+    public void test5() {
+        String result = Main.check(35);
+        assertEquals("12000 VND", result);
+
+    }
+
+}
